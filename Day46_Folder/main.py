218fcaf4ba4bdb74f9841b434d7fdd9286c69347
Timothy <78111482+tim0chiang@users.noreply.github.com>
2025-08-23 23:12:21 -0700

    Day 46
    Web scrap the top 100 songs of the year and used Spotify API to put it in to a playlist

--- Day46_Folder/main.py
+++ Day46_Folder/main.py
@@ -1,10 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import datetime as dt
 
-response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+ID = "example1"
+SECRET = "example2"
+BILLBOARD_URL = "https://www.billboard.com/charts/hot-100/"
+SPOTIFY_URL = "https://open.spotify.com/"
+REDIRECT_URL = "https://redurl.com/callback/"
+
+user_date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+if user_date == "":
+    user_date = dt.datetime.now()
+
+response = requests.get(url=f"{BILLBOARD_URL}{user_date}")
 soup = BeautifulSoup(response.text, "html.parser")
-all_movies = [movie.getText() for movie in soup.find_all(name="h3", class_="title")]
+song_titles = soup.find_all(name="h3",
+                            class_=re.compile("c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021"))
+all_songs = [song.getText().strip() for song in song_titles]
 
-with open("top_movies.txt", mode="w") as file:
-    for x in range(len(all_movies), 0, -1):
-        file.write(f"{all_movies[x-1]}\n")
+print(all_songs)
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID, client_secret=SECRET, redirect_uri=REDIRECT_URL,
+                                               scope="playlist-modify-private", show_dialog=True,
+                                               cache_path=".cache"))
+user_id = sp.current_user().get("id")
+
+year = user_date.split("-")[0]
+all_uri = []
+
+for song in all_songs:
+    result = sp.search(q=f"track: {song}", type="track", limit=1)
+    try:
+        all_uri.append(result["tracks"]["items"][0]["uri"])
+    except IndexError:
+        print("Song is not available on Spotify")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{user_date} Billboard 100", public=False)
+playlist_id = playlist["id"]
+
+sp.playlist_add_items(playlist_id=playlist_id, items=all_uri)
